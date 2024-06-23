d67bb622b4d33ff6e01560573517ae48117c35a7
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-06-23 23:13:41 +0300

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 
 site="https://www.ynetnews.com/category/3082"
-
 data=$(wget --no-check-certificate -q -O - $site 2>/dev/null)
-
 articles=$(echo "$data" | grep -oP "https://www.ynetnews.com/article/[0-9a-zA-Z]+")
-
 uniqarticles=$(echo "$articles" | sort | uniq)
 
 words=("Netanyahu" "Gvir" "Lapid" "Gantz")
 
-articles_dir="articles"
-
-mkdir -p $articles_dir
-
 for url in $uniqarticles; do 
+    article_content=$(wget --no-check-certificate -q -O - "$url")
 
-	article_content=$(wget --no-check-certificate -q -O - "$url")
+    output="$url"
 
-	
-	output="$url"
+    allzero=true  # Reset allzero flag for each article
 
-	allzero=true
+    for word in "${words[@]}"; do 
+        count=$(echo $article_content | grep -o -i "$word" | wc -l)
+        if [[ $count -ne 0 ]]; then 
+            allzero=false  # Set allzero to false if any word count is non-zero
+            output="$output, $word, $count"  # Include the name and count in output
+        fi
+    done
 
-	for word in "${words[@]}"; do 
-		count=$(echo $article_content | grep -o -i "$word" | wc -l)
-		if [[ $count -ne 0 ]]; then 
-			allzero=false
-			output="$output, $word, $count"
-		fi
-	done
+    if $allzero; then
+        output="$url, -"  # Set output to include a dash if no public figure is mentioned
+    fi
 
-if ! $allzero; then
-	output="$url, -"
-fi
-
-echo "$output"
+    echo "$output"  # Print the output line for each article
 done
-
-
-
-
-
