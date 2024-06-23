f00bea0befff8af8c8869033c18d7d55d3525a41
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-06-23 23:23:47 +0300

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -7,24 +7,27 @@ uniqarticles=$(echo "$articles" | sort | uniq)
 
 words=("Netanyahu" "Gvir" "Lapid" "Gantz")
 
+log_file="script_log.txt"  # Log file to track script execution
+
 for url in $uniqarticles; do 
-    article_content=$(wget --no-check-certificate -q -O - "$url")
+    # Retrieve article content
+    article_content=$(wget --no-check-certificate -q -O - "$url" 2>/dev/null)
 
     output="$url"
-
-    allzero=true  # Reset allzero flag for each article
+    nonzero=false  # Flag to track if any non zero count is found in the article
 
     for word in "${words[@]}"; do 
-        count=$(echo $article_content | grep -o -i "$word" | wc -l)
+        count=$(echo "$article_content" | grep -o -i "$word" | wc -l)
         if [[ $count -ne 0 ]]; then 
-            allzero=false  # Set allzero to false if any word count is non-zero
             output="$output, $word, $count"  # Include the name and count in output
+            nonzero=true  # Set mention_found to true if any mention is found
         fi
     done
 
-    if $allzero; then
-        output="$url, -"  # Set output to include a dash if no public figure is mentioned
+    if ! $nonzero; then
+        output="$url, -"  # Set output to include a dash if all zero
     fi
 
-    echo "$output"  # Print the output line for each article
-done
+    echo "$output"  
+
+done 
