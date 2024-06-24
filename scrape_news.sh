cac16a97bf6be1231e39e54047455e89762b01f9
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-06-24 17:55:19 +0300

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+# This script calculates how many times the words "Netanyahu" and "Gantz" appear in Ynet articles
 
-# Download main page
+# Download the main page content
 wget -q -O ynetnews "https://www.ynetnews.com/category/3082"
 
-# Extract URLs
-articles=$(grep -oP "https://www.ynetnews.com/article/[0-9a-zA-Z]+" ynetnews | sort | uniq)
+# Extract article URLs
+articles=$(grep -oP "https://(www\.)?ynetnews.com/article/[0-9a-zA-Z]+" ynetnews | sort | uniq)
+
+# Download all articles
+wget --no-check-certificate -q -i <(echo "$articles")
 
 # Process each URL
 for url in $articles; do
-    # Get article content
-    article_content=$(wget --no-check-certificate -q -O - "$url" 2>/dev/null)
-    
-    # Count keywords
-    N_count=$(echo "$article_content" | grep -o -i "Netanyahu" | wc -l)
-    G_count=$(echo "$article_content" | grep -o -i "Gantz" | wc -l)
-    
-    # Output result
+    # Extract the article filename from the URL
+    filename=$(echo "$url" | grep -o '[^/]\+$')
+
+    # Count occurrences of 'Netanyahu' and 'Gantz' in the article content
+    N_count=$(grep -o -i "Netanyahu" "$filename" | wc -l)
+    G_count=$(grep -o -i "Gantz" "$filename" | wc -l)
+
+    # Output the result
     if (( N_count == 0 && G_count == 0 )); then
         echo "$url, -"
     else
