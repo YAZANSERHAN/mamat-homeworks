1520bf96fe9c90f05489f8a2b0e5202702c04f59
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-06-24 19:21:26 +0300

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -5,15 +5,15 @@
 main_page_content=$(wget -q -O - "https://www.ynetnews.com/category/3082")
 
 # Extract unique article URLs
-articles=$(echo "$main_page_content" | grep -oP 'https://(www.)ynetnews.com/article/[0-9a-zA-Z]+' | sort | uniq)
+articles=$(echo "$main_page_content" | grep -oP 'https://(www\.)?ynetnews.com/article/[0-9a-zA-Z]+' | sort | uniq)
 
-# Process each article URL in parallel to optimize the process
+# Process each article URL in parallel to optimize the script
 for url in $articles; do
     {
         # Download the article content
         article_content=$(wget --no-check-certificate -q -O - "$url" 2>/dev/null)
 
-        # Count occurrences of 'Netanyahu' and 'Gantz' using a single grep call and process substitution
+        # Count occurrences of 'Netanyahu' and 'Gantz'
         N_count=$(echo "$article_content" | grep -o -i "Netanyahu" | wc -l)
         G_count=$(echo "$article_content" | grep -o -i "Gantz" | wc -l)
 
