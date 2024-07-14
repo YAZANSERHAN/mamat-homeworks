bf98b315e7338f1ac2c85618dd1d94c8000400c6
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-07-14 17:39:05 +0300

    Update calc_stat.sh

--- calc_stat.sh
+++ calc_stat.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
+# Check the number of arguments
 if [ $# -ne 1 ]; then
     echo "Wrong number of arguments" >&2
     exit 1
 fi
 
+# Validate the course number format
 if ! [[ "$1" =~ ^[0-9]{6}$ ]]; then
     echo "Course not found" >&2
     exit 1
@@ -14,12 +15,12 @@ fi
 courseNo=$1
 found=false
 
+# Search for the course directory
 for dir in */; do
-
   dir=${dir%/}
-  
-  if [[ $dir = $courseNo ]]; then
+  if [[ $dir == $courseNo ]]; then
     found=true
+    break
   fi
 done
 
@@ -28,21 +29,29 @@ if [ "$found" = false ]; then
     exit 1
 fi
 
-# checks if stat dir exists, and it deletes it if it does
-stat_dir="$courseNo"_stat
-
+# Check if the statistics directory exists and remove it if it does
+stat_dir="${courseNo}_stat"
 if [ -d "$stat_dir" ]; then
   rm -rf "$stat_dir"
 fi
 
-# creates new stat dir
+# Create a new statistics directory
 mkdir "$stat_dir"
 
-./hist $courseNo/grades.txt > $stat_dir/histogram.txt
+# Generate histogram
+./hist "$courseNo/grades.txt" > "$stat_dir/histogram.txt"
 
-mean="./mean $courseNo/grades.txt"
-median="./median $courseNo/grades.txt"
-min="./min $courseNo/grades.txt"
-max="./max $courseNo/grades.txt"
+# Calculate statistics and store the output
+mean=$(./mean "$courseNo/grades.txt")
+median=$(./median "$courseNo/grades.txt")
+min=$(./min "$courseNo/grades.txt")
+max=$(./max "$courseNo/grades.txt")
 
-echo -e "$mean\t$median\t$min\t$max" > $stat_dir/statistics.txt
+# Check if all commands executed successfully
+if [ $? -ne 0 ]; then
+    echo "Error calculating statistics" >&2
+    exit 1
+fi
+
+# Write statistics to the file
+echo -e "$mean\t$median\t$min\t$max" > "$stat_dir/statistics.txt"
