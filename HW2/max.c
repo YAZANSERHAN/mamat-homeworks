a0b310721258057470941ad2c02292089635bd3d
YAZANSERHAN <yazan.sarhan@campus.technion.ac.il>
2024-07-17 01:45:33 +0300

    Update max.c

--- HW2/max.c
+++ HW2/max.c
@@ -7,49 +7,51 @@
 
 void max(FILE *fd); 
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     FILE *fd;
 
-    if(argc == 1 || !strcmp("-", argv[1])){
+    if (argc == 1 || !strcmp("-", argv[1])) {
         fd = stdin;
     } else { 
         fd = fopen(argv[1], "r");
     }
-    if(!f){
+
+    if (!fd) {
         fprintf(stderr, "File not found: \"%s\"\n", argv[1]);
         return 1;
-         }   
-    max(f);
+    }   
+
+    max(fd);
+
     if (fd != stdin) {
         fclose(fd);
     }
+
     return 0;
 }
 
-void max(FILE *fd){
-   int line_number=1;
-   int grade;
-   int max_grade=MIN;
-   int counter;
-   int retval;
-   
+void max(FILE *fd) {
+    int line_number = 1;
+    int grade;
+    int max_grade = MIN;
+    int retval;
 
-    while(1){
+    while (1) {
         retval = fscanf(fd, "%d", &grade);
-        if(retval == EOF){
+        if (retval == EOF) {
             break;
-        }
-        else if(retval != 1){ 
-            fprintf(stderr, "Error: not a number\n");
+        } else if (retval != 1) { 
+            fprintf(stderr, "Error: not a number at line %d\n", line_number);
             exit(1);
-        }
-        else if(grade > MAX || grade < MIN){
-        fprintf(stderr, "Error at line "%d": grade "%d" \n",)line_number, grade;
-        }
-        if(max_grade<grade){
-            max_grade=grade;
+        } else if (grade > MAX || grade < MIN) {
+            fprintf(stderr, "Error at line %d: grade %d invalid\n", line_number, grade);
+        } else {
+            if (max_grade < grade) {
+                max_grade = grade;
+            }
         }
         line_number++;
     }
-    fprintf(stdout,"%d",max_grade);
+
+    fprintf(stdout, "%d\n", max_grade);
 }
